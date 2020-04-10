1fa8704199660043a12a862bbf057d576299ab75
rbo5046@psu.edu <rbo5046@psu.edu>
2020-04-09 22:34:43 -0400

    Add email sender

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const Airtable = require('airtable');
 const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
 const functions = require('./functions');
 const PORT = process.env.PORT || 5000;
@@ -144,6 +145,61 @@ app.post('/api/reviews', (req, res) => {
       res.status(200).send();
     }
   });
+});
+
+app.post('/api/email', async(req, res) => {
+  const base = new Airtable({apiKey: process.env.VUE_APP_AIRTABLE_API_KEY}).base('appJF67FB8VuGSvDx');
+
+  let resolveEmails = new Promise((resolve, reject) => {
+      let emailList = [];
+      base('email-contact-list').select({
+        view: "Grid view"
+      }).eachPage(function page(emails, fetchNextPage) {
+
+          // This function (`page`) will get called for each page of records.
+          emailList.push(...emails);
+
+          fetchNextPage();
+
+      }, function done(err) {
+          if (err) { 
+            reject(err);
+          } else {
+            emailList = emailList.map(record => {
+              return record.fields.email;
+            });
+            resolve(emailList);
+        }
+      });
+  });
+
+  let emails = await resolveEmails;
+
+  if(req.body) {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+             user: 'climber.crowd.contact@gmail.com',
+             pass: 'climbingiscool123'
+         }
+     });
+
+     var mailOptions = {
+      from: 'climber.crowd.contact@gmail.com',
+      to: emails.toString(),
+      subject: `From: ${req.body.email} Subject: ${req.body.subject}`,
+      text: req.body.content,
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        res.status(500).send();
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).send();
+      }
+    });
+  }
 });
 
 app.listen(PORT, () => {
